ef4123751fc2efd0ac8dc82398d0bafde6397805
mwbullen <mwbullen@gmail.com>
2016-01-14 11:03:31 -0500

    Added chests that open based on time

---
+++ Ghost Town + UFPS + Zombies/Assets/Container.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Container : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Open()
+    {
+        PlayMakerFSM mainFSM = GetComponent<PlayMakerFSM>();
+
+        mainFSM.SendEvent("Open");
+    }
+}
