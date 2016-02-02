2f93540246023d201a71d30120e7560bfb403646
mwbullen <mwbullen@gmail.com>
2016-02-01 20:12:32 -0500

    adding moviebackground to menu scene

---
+++ Ghost Town + UFPS + Zombies/Assets/movieBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class movieBackground : MonoBehaviour {
+
+	public Texture movieTexture;
+	MovieTexture m;
+
+	// Use this for initialization
+	void Start () {
+		m = (MovieTexture)GetComponent<RawImage> ().texture;
+		m.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
