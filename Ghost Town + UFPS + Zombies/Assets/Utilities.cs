338af315c9674312d299c8327be001975edd510c
mwbullen <mwbullen@gmail.com>
2016-02-08 16:53:43 -0500

    Added line of sight check when zombies attack

---
+++ Ghost Town + UFPS + Zombies/Assets/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utilities : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool canISeeGameObject(GameObject targetObject) {
+		RaycastHit r;
+
+		Vector3 angleToPlayer = transform.position = targetObject.transform.position;
+
+		Physics.Raycast (transform.position, angleToPlayer, out r);
+
+		Collider hitCollider = r.collider;
+		GameObject hitGameObject = hitCollider.gameObject;
+
+		return (hitGameObject == targetObject);
+	}
+}
